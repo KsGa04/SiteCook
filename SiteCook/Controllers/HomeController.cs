b5deeee10d42f8aaf2ff3dc3f362d1b57be4a693
Ksenia <kseniagaranceva@gmail.com>
2023-05-25 22:30:38 +0300

    Добавление категории, блюд, модератора

--- SiteCook/Controllers/HomeController.cs
+++ SiteCook/Controllers/HomeController.cs
@@ -28,13 +28,17 @@ namespace SiteCook.Controllers
         [HttpPost]
         public IActionResult Authorization(string loginOrEmail, string password, string role)
         {
-            if (role == "Админ") {
+            if (role == "Администратор") {
                 using (CookingBookContext db = new CookingBookContext())
                 {
                     Administrator? administrator = (from o in db.Administrators where o.Mail == loginOrEmail && o.Password == password select o).FirstOrDefault();
                     if (administrator is not null)
                     {
-                        return RedirectToAction("Index");
+                        var claims = new List<Claim> { new Claim(ClaimTypes.NameIdentifier, administrator.IdAdministrator.ToString()), new Claim(ClaimsIdentity.DefaultRoleClaimType, "Administrator") };
+                        ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, "Cookies");
+
+                        HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+                        return RedirectToAction("AddingCategory");
                     }
                     else
                     {
@@ -50,7 +54,11 @@ namespace SiteCook.Controllers
                     Moderator? moderator = (from o in db.Moderators where o.Mail == loginOrEmail && o.Password == password select o).FirstOrDefault();
                     if (moderator is not null)
                     {
-                        return RedirectToAction("Stat");
+                        var claims = new List<Claim> { new Claim(ClaimTypes.NameIdentifier, moderator.IdModerator.ToString()), new Claim(ClaimsIdentity.DefaultRoleClaimType, "Moderator")};
+                        ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, "Cookies");
+
+                        HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+                        return RedirectToAction("PrivateAccountModer");
                     }
                     else
                     {
@@ -110,6 +118,11 @@ namespace SiteCook.Controllers
                 }
                 else
                 {
+                    User user = new User();
+                    user.Mail = email;
+                    user.Password = password;
+                    db.Add(user);
+                    db.SaveChanges();
                     return View("Authorization");
                 }
             }
@@ -131,9 +144,17 @@ namespace SiteCook.Controllers
         {
             return View();
         }
+        [Authorize(Roles = "User")]
         public IActionResult PrivateAccount()
         {
-            return View();
+            var user = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier);
+            int id = Convert.ToInt32(user.Value);
+            using (CookingBookContext db = new CookingBookContext())
+            {
+                User currentUser = db.Users.Where(e => e.IdUser == id).FirstOrDefault();
+                return View("PrivateAccount", currentUser);
+            }
+            
         }
         public RedirectToActionResult SaveChangesUser(string loginOrEmail, string password, string nik, DateTime Data)
         {
@@ -156,7 +177,101 @@ namespace SiteCook.Controllers
         {
             return View();
         }
-        
+
+        [Authorize(Roles = "Moderator")]
+        public IActionResult PrivateAccountModer()
+        {
+            var moder = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier);
+            int id = Convert.ToInt32(moder.Value);
+            using (CookingBookContext db = new CookingBookContext())
+            {
+                Moderator currentModerator = db.Moderators.Where(e => e.IdModerator == id).FirstOrDefault();
+                return View("PrivateAccountModer", currentModerator);
+            }
+
+        }
+        public RedirectToActionResult SaveChangesModer(string loginOrEmail, string password, string nik, DateTime Data)
+        {
+            var claim = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier);
+            int id = Convert.ToInt32(claim.Value);
+            using (CookingBookContext db = new CookingBookContext())
+            {
+                Moderator currentModer = db.Moderators.Where(e => e.IdModerator == id).FirstOrDefault();
+                currentModer.Mail = loginOrEmail;
+                currentModer.Password = password;
+                currentModer.NikName = nik;
+                currentModer.DateOfBirth = Data;
+                db.SaveChanges();
+            }
+
+            ViewBag.Message = string.Format("Данные были сохранены");
+            return RedirectToAction("PrivateAccountModer");
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public IActionResult AddingCategory()
+        {
+            return View();
+        }
+        public RedirectToActionResult AddCategory(string nameCategory, IFormFile photo)
+        {
+            MemoryStream ms = new MemoryStream();
+            photo.CopyTo(ms);
+            using (CookingBookContext db = new CookingBookContext())
+            {
+                Category category = new Category();
+                category.NameCategory = nameCategory;
+                category.ImageCategory = ms.ToArray();
+                db.Add(category);
+                db.SaveChanges();
+            }
+            return RedirectToAction("AddingCategory");
+        }
+        [Authorize(Roles = "Administrator")]
+        public IActionResult AddingMeal()
+        {
+            return View();
+        }
+        public RedirectToActionResult AddMeal(string nameMeal, string desc, string namecateg, IFormFile photo)
+        {
+            MemoryStream ms = new MemoryStream();
+            photo.CopyTo(ms);
+            using (CookingBookContext db = new CookingBookContext())
+            {
+                var categ = db.Categories.Where(x => x.NameCategory == namecateg).FirstOrDefault();
+                int id = categ.IdCategory;
+                Meal meal = new Meal();
+                meal.NameMeal = nameMeal;
+                meal.ImageMeal = ms.ToArray();
+                meal.DescriptionMeal = desc;
+                meal.IdCategory = id;
+                db.Add(meal);
+                db.SaveChanges();
+            }
+            return RedirectToAction("AddingMeal");
+        }
+        [Authorize(Roles = "Administrator")]
+        public IActionResult AddingModerator()
+        {
+            return View();
+        }
+        public RedirectToActionResult AddModerator(string email, string pass, string nickname, DateTime Data, string namecateg)
+        {
+            using (CookingBookContext db = new CookingBookContext())
+            {
+                var categ = db.Categories.Where(x => x.NameCategory == namecateg).FirstOrDefault();
+                int id = categ.IdCategory;
+                Moderator moderator = new Moderator();
+                moderator.Mail = email;
+                moderator.Password = pass;
+                moderator.NikName = nickname;
+                moderator.DateOfBirth = Data;
+                moderator.IdCategory = id;
+                db.Add(moderator);
+                db.SaveChanges();
+            }
+            return RedirectToAction("AddingModerator");
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
